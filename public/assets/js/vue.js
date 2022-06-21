3a480cd77d3b59cfc1ae193302ce7966706724ed
mashoun <mashoun.me@gmail.com>
2022-06-21 23:03:01 +0300

    v1

---
+++ public/assets/js/vue.js
@@ -0,0 +1,58 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            spinner: false,
+            tab1: true,
+            name: '',
+            subject: '',
+            message: '',
+            email: '',
+            newsletter:'',
+            tab2: true,
+            tab3: true,
+            profile: '',
+            printing: '',
+            rollingPapers: '',
+            packaging: '',
+            api2: 'https://script.google.com/macros/s/AKfycbx5JolRUyxD6qzlPbNL6lRKnuX7CdpNzNyB-A4VslNYsO4REFscDmx3QL88ckWvjHKpXA/exec',
+            api: 'https://script.google.com/macros/s/AKfycbxE7GupR4KNVvJfoDeL8xWWC_WuN_uwQB6wNri2F8p1Kn-UCXIYku4-LQSTq4z0kmzNLQ/exec'
+
+        }
+    },
+    methods: {
+        encode(x) {
+            return encodeURIComponent(x)
+        },
+        postNews() {
+            fetch(this.api2, {
+                method: 'POST', // or 'PUT'
+                headers: {
+                    'Content-Type': 'text/plain',
+                },
+                body: this.newsletter
+            }).then(res => res.json()).then(res => {
+                alert(res)
+            })
+        },
+        tabAll() {
+            this.tab1 = true;
+            this.tab2 = true;
+            this.tab3 = true;
+        }
+    },
+    mounted() {
+        this.spinner = true;
+        fetch(this.api).then(res => res.json()).then(res => {
+            console.log(res)
+            this.profile = res
+            this.printing = res.products.printing
+            this.rollingPapers = res.products.rollingPapers
+            this.packaging = res.products.packaging
+
+            console.log(this.packaging)
+            this.spinner = false
+        })
+    }
+}).mount('#appp')
